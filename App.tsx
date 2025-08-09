0c25565157027e87364cc169909e2b04a83c823a
motoshira <51407064+motoshira@users.noreply.github.com>
2025-08-09 12:16:03 +0900

    Show image in SKia Canvas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+import { useWindowDimensions } from "react-native";
+import { Canvas, Image, useImage } from "@shopify/react-native-skia";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const sampleImage = useImage(require("./assets/sample.jpg"));
+	const { width, height } = useWindowDimensions();
+
+	return (
+		<View style={styles.container}>
+			<Canvas style={{ flex: 1 }}>
+				<Image
+					image={sampleImage}
+					fit="contain"
+					x={0}
+					y={0}
+					width={width}
+					height={height}
+				/>
+			</Canvas>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "stretch",
+		justifyContent: "center",
+	},
 });
